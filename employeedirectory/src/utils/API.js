01f352c8a03e60a3c2ff5c41d37cd535a387e1e4
Jason McDonald <jasonmcd65@gmail.com>
2021-03-12 21:55:09 -0600

    chnaged API call size

--- employeedirectory/src/utils/API.js
+++ employeedirectory/src/utils/API.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios"
 
-const BASEURL = "https://randomuser.me/api/?results=10"
+const BASEURL = "https://randomuser.me/api/?results=20"
 
 export default {
     getEmployee: function() {
