7d59d94c8996309b1f66de780a61b38ef706c1f7
Jason McDonald <jasonmcd65@gmail.com>
2021-03-12 21:56:00 -0600

    searches, sorts descending and sorts ascending

--- employeedirectory/src/components/ResultsContainer.js
+++ employeedirectory/src/components/ResultsContainer.js
@@ -4,18 +4,104 @@ import API from "../utils/API"
 class ResultsContainer extends Component {
 
     state = {
-        originalArray: [],
-        modifiedArray: [],
+        employeeArray: [],
+        filteredArray: [],
+        search: ""
+    }
+
+    updateSearch = (event) => {
+        this.setState({ search: event.target.value }, () => {
+            this.setState({
+                filteredArray: this.state.employeeArray.filter(
+                    emp => (emp.name.first.toLowerCase()).includes(this.state.search.toLowerCase())
+                )
+            })
+        })
     }
 
     componentDidMount() {
-        API.getEmployee().then(res => console.log(res.data.results))
+        API.getEmployee().then(res => {
+            this.setState({ employeeArray: res.data.results })
+            this.setState({ filteredArray: res.data.results })
+            console.log(this.state.employeeArray)
+        })
+    }
+
+    sortDescending(){
+
+        function compare(a, b) {
+            const nameA = a.name.first.toLowerCase();
+            const nameB = b.name.first.toLowerCase();
+
+            let comparison = 0;
+            if((nameA > nameB)){
+                comparison = 1;
+            }else if (nameA < nameB){
+                comparison = -1;
+            }
+
+            return comparison;
+        }
+
+        let array = this.state.filteredArray.sort(compare);
+        this.setState({filteredArray: array})
+
+    }
+
+    sortAscending(){
+        function compare(a, b) {
+            const nameA = a.name.first.toLowerCase();
+            const nameB = b.name.first.toLowerCase();
+            let comparison = 0;
+            if((nameA < nameB)){
+                comparison = 1;
+            }else if (nameA > nameB){
+                comparison = -1;
+            }
+
+            return comparison;
+        }
+
+        let array = this.state.filteredArray.sort(compare);
+        this.setState({filteredArray: array})
+
     }
 
     render() {
         return (
             <div>
-                <h2>Table</h2>
+                <input type="text" value={this.state.search} placeholder="Search By First Name" onChange={this.updateSearch}></input>
+                <button onClick={this.sortDescending.bind(this)}>Name Decending</button>
+                <button onClick={this.sortAscending.bind(this)}>Name Ascending</button>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Profile Picture</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.filteredArray.map(({
+                                picture,
+                                name,
+                                email,
+                                cell }) => {
+
+                                return (
+                                    <tr>
+                                        <th> <img src={picture.large} alt="Emoployee"/> </th>
+                                        <td>{name.first} {name.last}</td>
+                                        <td>{email}</td>
+                                        <td>{cell}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
             </div>
         )
     }
